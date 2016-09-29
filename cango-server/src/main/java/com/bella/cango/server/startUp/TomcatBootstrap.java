1d8a0eb1da5d297c0f2f01fc1e5185a749c45628
kevinKaiF <muzibella5144@163.com>
2016-09-29 23:05:25 +0800

    set system properties

--- cango-server/src/main/java/com/bella/cango/server/startUp/TomcatBootstrap.java
+++ cango-server/src/main/java/com/bella/cango/server/startUp/TomcatBootstrap.java
@@ -1,6 +1,7 @@
 package com.bella.cango.server.startUp;
 
 import com.bella.cango.service.CangoManagerService;
+import com.bella.cango.utils.PropertiesFileLoader;
 import org.apache.catalina.Container;
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
@@ -9,6 +10,7 @@ import org.apache.catalina.core.AprLifecycleListener;
 import org.apache.catalina.core.JreMemoryLeakPreventionListener;
 import org.apache.catalina.core.StandardServer;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.coyote.http11.Http11NioProtocol;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,7 +31,7 @@ public class TomcatBootstrap {
     private static final Logger LOGGER = LoggerFactory.getLogger(TomcatBootstrap.class);
 
     public static void main(String[] args) throws LifecycleException, ServletException {
-        System.getProperty("tomcat.util.scan.StandardJarScanFilter.jarsToSkip", "*.jar");
+        setSystemProperties();
         int port = Integer.parseInt(System.getProperty("cango.port", "8080"));
         String contextPath = System.getProperty("cango.contextPath", "/cango");
         String docBase = System.getProperty("cango.docBase", getDefaultDocBase());
@@ -47,6 +49,14 @@ public class TomcatBootstrap {
             }
         });
         tomcat.getServer().await();
+    }
+
+    private static void setSystemProperties() {
+        System.setProperty("tomcat.util.scan.StandardJarScanFilter.jarsToSkip", "*.jar");
+        String logHome = System.getProperty("cango.logHome");
+        if (StringUtils.isEmpty(logHome)) {
+            System.setProperty("cango.logHome", PropertiesFileLoader.DEFAULT_LOG_HOME);
+        }
     }
 
     /**
@@ -70,10 +80,10 @@ public class TomcatBootstrap {
     }
 
     private static Tomcat createTomcat(int port, String contextPath, String docBase) throws ServletException {
-        String tmpdir = System.getProperty("java.io.tmpdir");
+        String tmpDir = System.getProperty("java.io.tmpdir");
         Tomcat tomcat = new Tomcat();
-        tomcat.setBaseDir(tmpdir);
-        tomcat.getHost().setAppBase(tmpdir);
+        tomcat.setBaseDir(tmpDir);
+        tomcat.getHost().setAppBase(tmpDir);
         tomcat.getHost().setAutoDeploy(false);
         tomcat.getEngine().setBackgroundProcessorDelay(-1);
         tomcat.setConnector(newNioConnector());
@@ -93,8 +103,6 @@ public class TomcatBootstrap {
     }
 
     private static String getDefaultDocBase() {
-        File classpathDir = new File(Thread.currentThread().getContextClassLoader().getResource(".").getFile());
-        File projectDir = classpathDir.getParentFile().getParentFile();
-        return new File(projectDir, "src/main/webapp").getPath();
+        return new File(PropertiesFileLoader.PROJECT_DIR, "src/main/webapp").getPath();
     }
 }
